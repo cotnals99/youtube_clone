aac68d4ad61d5d16f716ca79eee3a9a1a7f565b9
cotnals99 <su.chae@icloud.com>
2022-09-07 11:41:28 +0700

    Routes, Components, NavBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from 'react'
+// import React from 'react'
 
-const app = () => {
-  return (
-    <div>app</div>
-  )
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from '@mui/material';
 
-export default app
+
+//Old Style importing
+// import Navbar from './components/Navbar';
+// import VideoDetail from './components/VideoDetail';
+// import ChannelDetail from './components/ChannelDetail';
+// import SearchFeed from './components/SearchFeed';
+// import { Feed } from '@mui/icons-material';
+
+//New Style
+import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
+
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{backgroundColor: '#000'}}>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Feed />} />
+        <Route path="/Video/:id" element={<VideoDetail />}/>
+        <Route path="/Channel/:id" element={<ChannelDetail />}/>
+        <Route path="/Search/:searchTerm" element={<SearchFeed />}/>
+      </Routes>
+    </Box>
+  </BrowserRouter>
+)
+
+export default App
+
